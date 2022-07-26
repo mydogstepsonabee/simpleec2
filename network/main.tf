86044d31f512688cb4d5c2aaa9787ea21f29efd2
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-07-26 11:07:59 +0900

    modify

--- network/main.tf
+++ network/main.tf
@@ -135,32 +135,4 @@ resource "aws_s3_bucket_policy" "vpc" {
 }
 POLICY
 }
-resource "aws_flow_log" "vpc" {
-  vpc_id          = "${aws_vpc.vpc.id}"
-  iam_role_arn    = "<iam_role_arn>"
-  log_destination = "${aws_s3_bucket.vpc.arn}"
-  traffic_type    = "ALL"
 
-  tags = {
-    GeneratedBy      = "Accurics"
-    ParentResourceId = "aws_vpc.vpc"
-  }
-}
-resource "aws_s3_bucket" "vpc" {
-  bucket        = "vpc_flow_log_s3_bucket"
-  acl           = "private"
-  force_destroy = true
-
-  versioning {
-    enabled    = true
-    mfa_delete = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
