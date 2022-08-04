fe2a9e8136157723d6fb2fe8b41cfe4a68a7c17f
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-08-04 19:06:14 +0900

    modify ec2 monitoring to true

--- ec2/main.tf
+++ ec2/main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "node" {
      http_endpoint = "disabled"
      http_tokens = "required"
    }
-   monitoring = false
+   monitoring = true
    
   user_data = file("${path.root}/ec2/userdata.tpl")
 
