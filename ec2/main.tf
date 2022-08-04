464a198fa7b3a3ba7f4c349b2dbedd37a81deea1
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-08-04 19:08:26 +0900

    modify ec2 monitoring to fale

--- ec2/main.tf
+++ ec2/main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "node" {
      http_endpoint = "disabled"
      http_tokens = "required"
    }
-   monitoring = true
+   monitoring = false
    
   user_data = file("${path.root}/ec2/userdata.tpl")
 
