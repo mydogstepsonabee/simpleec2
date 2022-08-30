268bd5852fa87921e548b1e5946545fa4d512a03
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-08-30 14:52:18 +0900

    remove comments

--- ec2/main.tf
+++ ec2/main.tf
@@ -26,7 +26,7 @@ data "cloudinit_config" "user_data" {
 
 # Create a EC2 Instance (Ubuntu 20)
 resource "aws_instance" "node" {
-  instance_type          = "t2.micro" # free instance
+  instance_type          = "t2.micro"
   ami                    = "ami-0cff7528ff583bf9a"
   key_name               = aws_key_pair.key_pair.id
   vpc_security_group_ids = [var.public_sg]
@@ -36,7 +36,6 @@ resource "aws_instance" "node" {
     Name = "TF Generated EC2"
   }
 
-  #user_data = file("${path.root}/ec2/userdata.tpl")
   user_data = data.cloudinit_config.user_data.rendered
 
   root_block_device {
