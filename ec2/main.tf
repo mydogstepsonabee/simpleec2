88bf949c0026dbff4c204b52005935ea9281f63e
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-09-27 14:57:01 +0900

    remove IDMv2

--- ec2/main.tf
+++ ec2/main.tf
@@ -47,17 +47,6 @@ resource "aws_instance" "node" {
     volume_size = 10
   }
 
-  #metadata_options {
-  # http_endpoint = "disabled"
-  # http_tokens   = "required"
-  #}
-  #monitoring = true
-
-
-  metadata_options {
-    http_endpoint = "disabled"
-    http_tokens   = "required"
-  }
 }
 
 # Create and assosiate an Elastic IP
