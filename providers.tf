2f7be9f7946fb2799f0715ea0f9891834ab8f3d0
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-09-27 17:09:44 +0900

    reverse test

--- providers.tf
+++ providers.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "aws" {
   region                   = var.aws_region
-  //shared_credentials_files = ["/Users/hxue/.aws/credentials"]
-  //profile                  = "kaisya"
+  shared_credentials_files = ["/Users/hxue/.aws/credentials"]
+  profile                  = "kaisya"
 }
