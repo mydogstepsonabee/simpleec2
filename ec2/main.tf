50bdb576379eb13eb916bfd3a1c610d633840b96
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-09-27 18:45:54 +0900

    add userdata

--- ec2/main.tf
+++ ec2/main.tf
@@ -13,7 +13,7 @@ resource "aws_key_pair" "key_pair" {
     command = "echo '${tls_private_key.key.private_key_pem}' > ./key.pem"
   }
 }
-/*
+
 data "cloudinit_config" "user_data" {
   gzip          = true
   base64_encode = true
@@ -23,7 +23,7 @@ data "cloudinit_config" "user_data" {
     filename     = "userdata.sh"
   }
 }
-*/
+
 # Create a EC2 Instance (Amazon EC2 with docker pulling image)
 resource "aws_instance" "node" {
   instance_type          = "t2.micro"
