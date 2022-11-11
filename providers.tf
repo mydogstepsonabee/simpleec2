ba74ad9968c7b20823c58be1f9f21b94c7a781df
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2022-11-11 12:33:37 +0900

    run terraform local

--- providers.tf
+++ providers.tf
@@ -1,10 +1,11 @@
 terraform {
-  cloud {
+ /* cloud {
     organization = "tcslimited"
     workspaces {
       name = "project1"
     }
   }
+  */
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -14,6 +15,6 @@ terraform {
 
 provider "aws" {
   region                   = var.aws_region
-  //shared_credentials_files = ["/Users/hxue/.aws/credentials"]
-  //profile                  = "kaisya"
+  shared_credentials_files = ["/Users/hxue/.aws/credentials"]
+  profile                  = "kaisya"
 }
